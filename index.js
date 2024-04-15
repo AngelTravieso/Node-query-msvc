4c1d7672807eff3940b032a5864275bcfd1ff6fc
AngelTravieso <angeltraviesoc@gmail.com>
2024-04-15 16:51:09 -0400

    Parsing Incoming Events

--- index.js
+++ index.js
@@ -7,11 +7,48 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = {};
+// QUICK EXAMPLE
+// post === {
+//     'asdas': {
+//         id: 'asdas'.replace,
+//         title: 'pot title',
+//         comments: [
+//             { id: 'alskdsa', content' comment!', }
+//         ]
+//     },
+//     'asdas': {
+//         id: 'asdas'.replace,
+//         title: 'pot title',
+//         comments: [
+//             { id: 'alskdsa', content' comment!', }
+//         ]
+//     },
+// }
+
 app.get('/posts', (req, res) => {
-    
+    res.send(posts);
 });
 
 app.post('/events', (req, res) => {
+    const { type, data } = res.body.type;
+
+    if(type === 'PostCreated') {
+        const { id, title } = data;
+
+        posts[id] = { id, title, comments: [], };
+    }
+
+    if(type === 'CommentCreated') {
+        const { id, content, postId } = data;
+        
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+
+    console.log(posts);
+
+    res.send({});
 
 });
 
